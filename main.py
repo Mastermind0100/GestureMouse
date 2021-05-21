445f060982d12cfeb42c32b1a7e51565183b0298
Atharva Hudlikar <atharvahudlikar@gmail.com>
2021-05-21 19:15:56 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import cv2
+import math
 import pyautogui as pag
 import handdetector as hdr
 
@@ -14,14 +15,17 @@ if __name__ == '__main__':
     while True:
         ret, inp_frame = cap.read()
         frame = cv2.flip(inp_frame, 1)
-        handLandmarks = handDetector.findHandLandMarks(image = frame, draw = True)
+        handLandmarks = handDetector.findHandLandMarks(image = frame, draw = False)
         if len(handLandmarks)==0:
             continue
-        x_crdnt, y_crdnt = handLandmarks[8][1], handLandmarks[8][2]
+        x_crdnt, y_crdnt = int((handLandmarks[8][1]+handLandmarks[4][1])/2), int((handLandmarks[8][2]+handLandmarks[4][2])/2)
+        dist = math.sqrt(pow((handLandmarks[8][1]-handLandmarks[4][1]),2)+pow((handLandmarks[8][2]-handLandmarks[4][2]),2))
         cv2.circle(frame, (x_crdnt, y_crdnt), 5, (0,255,255), -1)
-        cv2.imshow('frame', frame)                                          # You can comment this line out to not show your webcam output
+        cv2.line(frame, (handLandmarks[8][1],handLandmarks[8][2]), (handLandmarks[4][1],handLandmarks[4][2]), (255, 0, 255), 3)
+        cv2.imshow('frame', frame)       # You can comment this line out to not show your webcam output
         pag.moveTo(int(x_crdnt*multiplier_x), int(y_crdnt*multiplier_y), duration = 0.01)
-        
+        if dist<20:
+            pag.click(x_crdnt,y_crdnt)
         if cv2.waitKey(1) & 0xff == 27:
             break
     
